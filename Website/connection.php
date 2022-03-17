0ae4700b8a14f01ee7729b430bb3e45197189660
v80015aa <ahmed.almansoori@student.manchester.ac.uk>
2022-03-17 11:41:30 +0000

    Moved old HTML and CSS files

--- Website/connection.php
+++ Website/connection.php
@@ -1,11 +1,22 @@
 <?php
 
-$dbhost = "localhost";
-$dbusername = "root";
-$dbpassword = "876973145";
-$dbname = "unilife";
+#$dbhost = "localhost";
+#$dbusername = "root";
+#$dbpassword = "876973145";
+#$dbname = "unilife";
+
+$dbhost = "dbhost.cs.man.ac.uk";
+$dbusername = "v80015aa";
+$dbpassword = "123456781";
+#$dbname = "v80015aa";
+$dbname = array("2021_comp10120_y12",);
+
+
 
 if(!$con = mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname)){
     die("can't connect to database");
 }
+
+
+
 
--- Website/functions.php
+++ Website/functions.php
@@ -14,3 +14,5 @@ function check_login($con){
     header("GamePage.html"); //change here
     die;
 }
+
+
